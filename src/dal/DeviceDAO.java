6ab562d800bcef7bd616f0a289e584ce77026540
Bálint <47783658+Roharx@users.noreply.github.com>
2023-07-19 14:23:53 +0200

    Started DeviceDAO (will finish after work)

--- src/dal/DeviceDAO.java
+++ src/dal/DeviceDAO.java
@@ -1,11 +1,17 @@
 package dal;
 
 import bll.be.Device;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
 import dal.connector.MSSQLConnector;
 import dal.interfaces.IDeviceDAO;
+import dal.mapper.DeviceMapper;
 
 import java.io.IOException;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DeviceDAO implements IDeviceDAO {
@@ -22,17 +28,70 @@ public class DeviceDAO implements IDeviceDAO {
 
     @Override
     public List<Device> getAllDevicesForProject(String refNumber) {
-        return null;
+        DeviceMapper mapper = new DeviceMapper();
+        List<Device> allDevices = new ArrayList<>();
+
+        try {
+            Connection conn = mssqlConnector.createConnection();
+            String sql = "SELECT * FROM Devices";
+            preparedStatement = conn.prepareStatement(sql);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()){
+                mapper.mapDevice(rs);
+                allDevices.add(mapper.getDevice());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return allDevices;
     }
 
     @Override
-    public Device getDeviceByID(String refNumber, int id) {
-        return null;
+    public Device getDeviceByID(String refNumber, int id) { //TODO don't need refNumber
+        DeviceMapper mapper = new DeviceMapper();
+        Device device = null;
+
+        try {
+            Connection conn = mssqlConnector.createConnection();
+            String sql = "SELECT * FROM Devices WHERE id=?";
+            preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setInt(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()){
+                mapper.mapDevice(rs);
+                device = mapper.getDevice();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return device;
     }
 
     @Override
-    public Device getDeviceByName(String refNumber, String name) {
-        return null;
+    public Device getDeviceByName(String refNumber, String name) { //TODO check the DB
+        DeviceMapper mapper = new DeviceMapper();
+        Device device = null;
+
+        try {
+            Connection conn = mssqlConnector.createConnection();
+            String sql = "SELECT * FROM Devices WHERE refNumber=?, name=?";
+            preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, refNumber);
+            preparedStatement.setString(2, name);
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()){
+                mapper.mapDevice(rs);
+                device = mapper.getDevice();
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return device;
     }
 
     @Override
